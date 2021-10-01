e095e44eb208ec4a4c6ca8b0524c2ba794bc9b29
arsh16 <arsh16thind@gmail.com>
2021-10-01 17:55:55 -0400

    first

---
+++ .history/routes/index_20210930145135.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home', page: 'home' });
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home',  page: 'home'});
+});
+
+
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About',  page: 'about'});
+});
+
+router.get('/products', function(req, res, next) {
+  res.render('index', { title: 'Projects',  page: 'projects'});
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services', page: 'services' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact', page: 'contact' });
+});
+
+module.exports = router;
